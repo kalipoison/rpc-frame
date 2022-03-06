d97ddb726f3fcd7b71b9100ab78d1e6da0c5cb6f
kalipoison <1174616074@qq.com>
2022-03-06 14:20:26 +0800

    [3.0] 代码优化

--- rpc-core/src/main/java/com/gohb/rpc/serializer/ProtobufSerializer.java
+++ rpc-core/src/main/java/com/gohb/rpc/serializer/ProtobufSerializer.java
@@ -19,6 +19,7 @@ public class ProtobufSerializer implements CommonSerializer {
     private Map<Class<?>, Schema<?>> schemaCache = new ConcurrentHashMap<>();
 
     @Override
+    @SuppressWarnings("unchecked")
     public byte[] serialize(Object obj) {
         Class clazz = obj.getClass();
         Schema schema = getSchema(clazz);
@@ -32,6 +33,7 @@ public class ProtobufSerializer implements CommonSerializer {
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public Object deserialize(byte[] bytes, Class<?> clazz) {
         Schema schema = getSchema(clazz);
         Object obj = schema.newMessage();
@@ -44,6 +46,7 @@ public class ProtobufSerializer implements CommonSerializer {
         return SerializerCode.valueOf("PROTOBUF").getCode();
     }
 
+    @SuppressWarnings("unchecked")
     private Schema getSchema(Class clazz) {
         Schema schema = schemaCache.get(clazz);
         if (Objects.isNull(schema)) {
